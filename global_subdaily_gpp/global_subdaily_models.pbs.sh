7a34a67f0e974f90df0d44fdab0c5dfe0fde0d13
David Orme <davidorme@users.noreply.github.com>
2023-07-10 10:11:11 +0100

    Updating global subdaily model fitting scripts

---
+++ global_subdaily_gpp/global_subdaily_models.pbs.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=1:mem=40gb
+#PBS -J 1:720
+#PBS -j oe
+
+# The PBS lines above set the HPC job management details: 720 subjobs, each of which
+# needs one compute node with 1 cpu and 40GB of RAM, running for less than 1 day.
+
+BASEPATH="/rds/general/project/lab-prentice-realm-data/live/global_subdaily/"
+
+# Scaling:
+# - 720 longitudinal slices
+
+# Load python - requires xarray, dask, numpy, multiprocess
+module load anaconda3/personal
+source activate python3.10
+
+# Run the executable which picks up the subjob array index internally
+python3 $BASEPATH/global_subdaily_gpp/global_subdaily_models.py 
+
+# Tidy up
+conda deactivate
+module unload anaconda3/personal
--- global_subdaily_gpp/global_subdaily_models.py
+++ global_subdaily_gpp/global_subdaily_models.py
@@ -3,10 +3,14 @@
 This script is to be used as part of an array job on the HPC to iterate over blocks of
 cells from WFDE5 data in order to fit the subdaily P Model at global scale and 0.5°
 resolution.
+
+The PBS_ARRAY_INDEX environment variable changes within each subjob and is used to
+control which longitudinal slice is analysed by each subjob.
 """
 
 import os
 from pathlib import Path
+import time
 
 import xarray
 import numpy as np
@@ -14,19 +18,25 @@ import pandas as pd
 
 from pyrealm.hygro import convert_sh_to_vpd
 from pyrealm.pmodel.functions import calc_patm
-from pyrealm.pmodel import PModelEnvironment, FastSlowPModel, FastSlowScaler
+from pyrealm.pmodel import PModel, PModelEnvironment, FastSlowPModel, FastSlowScaler
 
 
 # ----------------------------------
 # DATA PATHS
 # ----------------------------------
+script_start = time.time()
 
 # WFDE5 provides TEMP, PATM, VPD, PPFD
 wfde_path = Path("/rds/general/project/lemontree/live/source/wfde5/wfde5_v2")
 # SNU FAPAR downsampled from 0.05° to 0.5° gives FAPAR
-fapar_path = Path("/rds/general/project/lemontree/live/derived/SNU_Ryu/FPAR_05d")
+fapar_path = Path("/rds/general/project/lemontree/live/derived/SNU_Ryu/FPAR2_05d")
 # NOAA global sea level time series provides CO2
 co2_path = Path("/rds/general/project/lemontree/live/source/NOAA_CO2/co2_mm_gl.csv")
+# Output directory
+output_path = Path(
+    "/rds/general/project/lab-prentice-realm-data/live/splash_CRU/"
+    "global_subdaily_gpp/results"
+)
 
 
 start_time = np.datetime64("2000-01-01")
@@ -83,12 +93,15 @@ idx_obj = (
         np.where(temp_source.time.values == start_time)[0][0],
         None,
     ),
-    slice(temp_source.sizes["lat"]),
+    slice(asurf_data.sizes["lat"]),
     slice(lon_idx, lon_idx + stripe_width),
 )
 
+# Retain the temperature xarray to use the coordinates for outputs.
+temp_xarray = temp_source["Tair"][idx_obj]
+
 # Extract temperature data and convert to °C
-temp_data = temp_source["Tair"][idx_obj].compute().data - 273.15
+temp_data = temp_xarray.compute().data - 273.15
 
 # Possible sources for atmospheric pressure
 use_constant_patm = True
@@ -109,17 +122,6 @@ else:
     patm_data = patm_source["PSurf"][idx_obj].compute().data
 
 # ----------------------------------
-# PPFD DATA
-# ----------------------------------
-
-# Find the downwelling shortwave radiation files and open them as a dataset
-swdown_files = list((wfde_path / "SWdown").rglob("*.nc"))
-swdown_source = xarray.open_mfdataset(swdown_files, chunks=chunks)
-
-# Extract shortwave downwelling radiation and convert to PPFD
-ppfd_data = swdown_source["SWdown"][idx_obj].compute().data * 2.04
-
-# ----------------------------------
 # VPD DATA
 # ----------------------------------
 
@@ -128,20 +130,57 @@ qair_files = list((wfde_path / "Qair").rglob("*.nc"))
 qair_source = xarray.open_mfdataset(qair_files, chunks=chunks)
 
 # Extract specific humidity and convert to VPD: kg kg-1 to Pa.
-qair_data = qair_source["SWdown"][idx_obj].compute().data
-vpd_data = convert_sh_to_vpd(sh=qair_data, ta=temp_data, patm=patm_data)
+qair_data = qair_source["Qair"][idx_obj].compute().data
+# Function takes pressure in kPa and returns kPa
+vpd_data = convert_sh_to_vpd(sh=qair_data, ta=temp_data, patm=patm_data / 1000) * 1000
+# Set negative values to zero
+vpd_data = np.clip(vpd_data, 0, np.inf)
+
+# ----------------------------------
+# PPFD DATA
+# ----------------------------------
+
+# Find the downwelling shortwave radiation files and open them as a dataset
+swdown_files = list((wfde_path / "SWdown").rglob("*.nc"))
+swdown_source = xarray.open_mfdataset(swdown_files, chunks=chunks)
+
+# SWDown has a different start datetime to other WFDE5 variables, so needs different
+# indexing
+idx_obj = (
+    slice(
+        np.where(swdown_source.time.values == start_time)[0][0],
+        None,
+    ),
+    slice(asurf_data.sizes["lat"]),
+    slice(lon_idx, lon_idx + stripe_width),
+)
+
+# Extract shortwave downwelling radiation and convert to PPFD
+ppfd_data = swdown_source["SWdown"][idx_obj].compute().data * 2.04
 
 # ----------------------------------
 # FAPAR DATA
 # ----------------------------------
 
 # Load the daily fAPAR and then convert to subdaily
-# NOTE - explicitly only getting files in C21 - problem with one file pre 2000
-fapar_files = list(fapar_path.rglob("halfd_FPAR_20*.nc"))
+fapar_files = list(fapar_path.rglob("*.nc"))
 fapar_source = xarray.open_mfdataset(fapar_files, chunks=chunks)
 
-# Extract shortwave downwelling radiation and convert to PPFD
-fapar_data = fapar_source["FPAR"][idx_obj].compute().data
+# Forward fill FAPAR to hourly sampling
+fapar_hourly = fapar_source["FPAR"].resample(time="1h").ffill()
+
+# Get the indices on the hourly resample
+idx_obj_fapar = (
+    slice(
+        np.where(fapar_hourly.time.values == start_time)[0][0],
+        np.where(fapar_hourly.time.values == end_time)[0][0],
+    ),
+    slice(asurf_data.sizes["lat"]),
+    slice(lon_idx, lon_idx + stripe_width),
+)
+
+# Extract the data
+fapar_data = fapar_hourly[idx_obj_fapar].compute().data
 
 # ----------------------------------
 # CO2 DATA
@@ -174,3 +213,104 @@ hourly_co2 = hourly_co2[
         np.where(hourly_co2.time.values == end_time)[0][0],
     )
 ]
+
+# Broadcast the data to the correct shape
+co2_data = np.broadcast_to(hourly_co2.data[:, np.newaxis, np.newaxis], temp_data.shape)
+
+print(
+    f"""
+Data loading finished after {script_start - time.time()} seconds:
+- lon_idx = {lon_idx}
+- temp_data.shape = {temp_data.shape}
+- patm_data.shape = {patm_data.shape}
+- vpd_data.shape = {vpd_data.shape}
+- co2_data.shape = {co2_data.shape}
+- fapar_data.shape = {fapar_data.shape}
+- ppfd_data.shape = {ppfd_data.shape}
+"""
+)
+
+# ----------------------------------
+# MODEL FITTING
+# ----------------------------------
+
+# Loop over the loaded longitudinal bands, correcting the datetimes from UTC to local
+# time for calculating subdaily representative times
+
+utc_times = temp_xarray.time.values
+lon_bands = np.arange(lon_idx, lon_idx + stripe_width)
+
+
+results = []
+
+for data_idx, this_lon_idx in enumerate(lon_bands):
+    # Indexer for the longitudinal band
+    lidx = (
+        slice(temp_xarray.sizes["time"]),
+        slice(asurf_data.sizes["lat"]),
+        slice(data_idx, data_idx + 1),
+    )
+
+    # Xarray coordinates for the band
+    band_coords = temp_xarray[lidx].coords
+
+    # Get the P Model environment
+    pm_env = PModelEnvironment(
+        tc=temp_data[lidx], patm=patm_data[lidx], vpd=vpd_data[lidx], co2=co2_data[lidx]
+    )
+
+    # Print out a data summary for the photosynthetic environment
+    pm_env.summarize()
+
+    # Fit the standard P Model
+    standard_pmod = PModel(pm_env, kphio=1 / 8)
+    standard_pmod.estimate_productivity(fapar=fapar_data[lidx], ppfd=ppfd_data[lidx])
+
+    # Print out a summary for the standard model
+    standard_pmod.summarize()
+
+    # Set up the local times - the lon idx in 0-719 defines 0.5° longitudinal bands,
+    # each of width is 2 minutes wide (24 / (720) * 60 = 2.0). So, find the local time
+    # for the left hand edge using this_lon_idx and add 1 minute to get the band centre.
+
+    local_time_delta = (24 * (this_lon_idx / 720) - 12) * 60 + 1
+    local_time_delta = np.timedelta64(local_time_delta, "M")
+    local_time = utc_times + local_time_delta
+
+    # Set a half hourly window around noon - with hourly data this is actually just
+    # picking the noon value.
+    fsscaler = FastSlowScaler(local_time)
+    fsscaler.set_window(
+        window_center=np.timedelta64(12, "h"),
+        half_width=np.timedelta64(1, "h"),
+    )
+
+    # Fit the subdaily P Model
+    fs_pmod = FastSlowPModel(
+        env=pm_env,
+        fs_scaler=fsscaler,
+        fapar=fapar_data[lidx],
+        ppfd=ppfd_data[lidx],
+        alpha=1 / 15,
+        kphio=1 / 8,
+    )
+
+    # Format and store the GPP data
+    res = xarray.Dataset(
+        {
+            "standard_gpp": xarray.DataArray(temp_data, coords=band_coords),
+            "subdaily_gpp": xarray.DataArray(temp_data, coords=band_coords),
+        }
+    )
+    results.append(res)
+
+    print(
+        f"Models fitted on lon_idx {this_lon_idx} after "
+        f"{script_start - time.time()} seconds"
+    )
+
+# Concatenate the results along the longitude axis and export
+out_data = xarray.concat(results, dim="lon")
+out_data.to_netcdf(output_path / f"gpp_data_{array_index}.nc")
+
+print(f"Script completed after {script_start - time.time()} seconds")
