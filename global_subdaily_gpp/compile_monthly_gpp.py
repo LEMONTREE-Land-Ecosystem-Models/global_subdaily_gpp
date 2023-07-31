a1d2eee8c4f057af058d2d3273e2c986374d879e
David Orme <davidorme@users.noreply.github.com>
2023-07-31 10:41:58 +0100

    Another attempt at a compilation file for monthly GPP files of the hourly estimates

--- global_subdaily_gpp/compile_monthly_gpp.py
+++ global_subdaily_gpp/compile_monthly_gpp.py
@@ -1,4 +1,5 @@
-"""This script takes the results files output by global_subdaily_models.py, which are
+"""
+This script takes the results files output by global_subdaily_models.py, which are
 0.5° longitudinal slices of a 20 year time series, and converts back to regular lat long
 grids. It partitions the time series into more manageable monthly output files.
 
@@ -37,47 +38,29 @@ year = array_index + 2000
 results_files = list(results_dir.rglob("gpp_data*.nc"))
 results_files = [(p, float(p.name[9:-3])) for p in results_files]
 results_files.sort(key=lambda x: x[1])
-results_files = [p for (p, lon) in results_files]
+
 
 start_time = time.time()
 print("Starting script")
 
 # Extract each month
-# Something bizarre happens here with trying to open the full set of 720 files using
-# open_mfdataset on the HPC. It very rapidly falls over with the message
-# "AttributeError: NetCDF: Not a valid ID". Running a smaller set of files seems to
-# succeed. For this reason, the loop below merges in a two step process.
-
-results_blocks = np.split(np.array(results_files), 4)
 
 for month in range(1, 13):
+    data = []
 
-    def _preprocess(ds):
-        return ds.sel(time=f"{year}-{month:02d}").drop_vars("local_time")
+    for this_file, lon in results_files:
+        # Open the source file using a with block to close cleanly and append the data
+        # for the month, dropping the local time variable, which changes across each
+        # longitudinal band.
+        with xarray.open_dataset(this_file) as ds:
+            data.append(ds.sel(time=f"{year}-{month:02d}").drop_vars("local_time"))
 
-    blocks = []
-
-    for idx, this_block in enumerate(results_blocks):
-        block_results = xarray.open_mfdataset(
-            this_block,
-            parallel=True,
-            preprocess=_preprocess,
-            chunks={"lat": 360, "lon": 720, "time": 1},
-            autoclose=True,
-        )
         print(
-            f"Year {year}, month {month:02d}, block {idx} opened"
+            f"Year {year}, month {month:02d}, lon {lon} loaded"
             f"after {time.time() - start_time} seconds"
         )
 
-        blocks.append(block_results.compute())
-
-        print(
-            f"Year {year}, month {month:02d}, block {idx} computed"
-            f"after {time.time() - start_time} seconds"
-        )
-
-    results_month = xarray.merge(blocks)
+    results_month = xarray.merge(data)
     results_month.to_netcdf(months_dir / f"gpp_data_{year}_{month:02d}.nc")
 
     print(
